cb896a8e8877a5f23688ee2c237595c52a70588b
Gun9niR <gun9nir.guo@gmail.com>
2021-04-16 16:26:02 +0800

    optimize bloom filter

--- header/BloomFilter.h
+++ header/BloomFilter.h
@@ -32,21 +32,21 @@ template<typename Key>
 void BloomFilter<Key>::put(const Key& key) {
     unsigned int hash[4] = {0};
     MurmurHash3_x64_128(&key, 8, 1, hash);
-    for (auto i : hash) {
-        filter[i % BLOOM_FILTER_SIZE] = true;
-    }
+
+    filter[hash[0] % BLOOM_FILTER_SIZE] =
+        filter[hash[1] % BLOOM_FILTER_SIZE] =
+            filter[hash[2] % BLOOM_FILTER_SIZE] =
+                filter[hash[3] % BLOOM_FILTER_SIZE] = true;
 }
 
 template<typename Key>
 bool BloomFilter<Key>::isProbablyPresent(const Key& key) const {
     unsigned int hash[4] = {0};
     MurmurHash3_x64_128(&key, 8, 1, hash);
-    for (auto i: hash) {
-        if (!filter[i % BLOOM_FILTER_SIZE]) {
-            return false;
-        }
-    }
-    return true;
+    return filter[hash[0] % BLOOM_FILTER_SIZE] &&
+            filter[hash[1] % BLOOM_FILTER_SIZE] &&
+            filter[hash[2] % BLOOM_FILTER_SIZE] &&
+            filter[hash[3] % BLOOM_FILTER_SIZE];
 }
 
 template<typename Key>
